e018b0668dbbcf2fff2882ff7bc11c8669106ba0
mirasokolova <mira.rosenova@gmail.com>
2023-01-11 09:00:33 +0200

    new

---
+++ 3tables_WebAuto/3tables_WebAuto/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3tables_WebAuto.Entities
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.Purchase = new HashSet<Purchase>();
+        }
+         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string LastNAme { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string FirstNAme { get; set; }
+    }
+}
