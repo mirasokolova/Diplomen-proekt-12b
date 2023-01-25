4ad221bc7f293eebc50cf691bf703e9c69bcb1f7
mirasokolova <mira.rosenova@gmail.com>
2023-01-25 08:59:27 +0200

    new

--- Dog_WebApp/Dog_WebApp/Controllers/DogsController.cs
+++ Dog_WebApp/Dog_WebApp/Controllers/DogsController.cs
@@ -11,37 +11,58 @@ namespace Dog_WebApp.Controllers
 {
     public class DogsController : Controller
     {
-        private readonly ApplicationDbContext context;
-
-        public DogsController(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(DogCreateViewModel bindingModel)
-        {
-            if (ModelState.IsValid)
+      
+        
+            private readonly ApplicationDbContext context;
+            public DogsController(ApplicationDbContext context)
             {
-                Dog dogFromDb = new Dog
-                {
-                    Name = bindingModel.Name,
-                    Age = bindingModel.Age,
-                    Breed = bindingModel.Breed,
-                    Image = bindingModel.Image,
-                };
-                context.Dogs.Add(dogFromDb);
-                context.SaveChanges();
-                return this.RedirectToAction("Success");
+                this.context = context;
             }
-            return this.View();
-        }
-        public IActionResult actionResult()
-        {
-            return View();
+            public IActionResult Index()
+            {
+                return View();
+            }
+            public IActionResult Create()
+            {
+                return this.View();
+            }
+
+            [HttpPost]
+            public IActionResult Create(DogCreateViewModel bindingModel)
+            {
+                if (ModelState.IsValid)
+                {
+                    Dog dogFromDb = new Dog
+                    {
+                        Name = bindingModel.Name,
+                        Age = bindingModel.Age,
+                        Breed = bindingModel.Breed,
+                        Image = bindingModel.Image,
+                    };
+                    context.Dogs.Add(dogFromDb);
+                    context.SaveChanges();
+
+                    return this.RedirectToAction("Success");
+                }
+                return this.View();
+            }
+
+            public IActionResult Success()
+            {
+                return this.View();
+            }
+
+            public IActionResult All()
+            {
+                List<DogAllViewModel> dogs = context.Dogs.Select(dogFromDb => new DogAllViewModel
+                {
+                    Id = dogFromDb.Id,
+                    Name = dogFromDb.Name,
+                    Age = dogFromDb.Age,
+                    Breed = dogFromDb.Breed,
+                    Image = dogFromDb.Image
+                }).ToList();
+                return View(dogs);
+            }
         }
     }
-}
