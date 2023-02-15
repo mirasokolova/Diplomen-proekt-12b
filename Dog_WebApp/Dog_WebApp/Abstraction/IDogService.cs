3053dd8a649ed7966c02d729664cead323f6cad4
mirasokolova <mira.rosenova@gmail.com>
2023-02-15 20:09:19 +0200

    new

---
+++ Dog_WebApp/Dog_WebApp/Abstraction/IDogService.cs
@@ -0,0 +1,19 @@
+﻿using Dog_WebApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dog_WebApp.Abstraction
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, string breed, string image);
+        bool UpdateDog(int dogId, string name, int age, string breed, string image);
+        List<Dog> GetDogs();
+        Dog GetDogById(int dogId);
+        bool RemoveById(int DogId);
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+        // object UpdateDog(string name, int age, string breed, string image);
+    }
+}
---
+++ Dog_WebApp/Dog_WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Dog_WebApp.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dog_WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+            Database.EnsureCreated();
+
+        }
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<Dog_WebApp.Models.DogCreateViewModel> DogCreateViewModel { get; set; }
+        public DbSet<Dog_WebApp.Models.DogAllViewModel> DogAllViewModel { get; set; }
+        public DbSet<Dog_WebApp.Models.DogDetailsViewModel> DogDetailsViewModel { get; set; }
+    }
+}
